e773e6e4e1d2d6e70a2df1e280c838124f510005
rahul4172 <rahulshelke4172@gmail.com>
2023-04-16 23:32:39 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello React</h1>
+    <div className="App bg-gradient-to-r from-yellow-300 to-orange-500" >
+     <header className="text-center">
+      <h1>James's Todo List </h1>
+     </header>
     </div>
   );
 }
