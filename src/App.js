c9671beb947b11a06d683ac1bf2ef059570d96b3
rahul4172 <rahulshelke4172@gmail.com>
2023-05-05 23:54:26 +0530

    set todos

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import React , {useState} from 'react'
 function App() {
 
   const [inputText , setInputtext] = useState(""); 
+  const[todos,setTodos] = useState([]);
 
 
   return (
     <div className="App bg-gradient-to-r from-yellow-300 to-orange-500 " >
      <header className="text-center">
-      <h1 className='text-center font-semibold text-3xl my-4 '>James's Todo List {inputText}</h1>
+      <h1 className='text-center font-semibold text-3xl my-4 '>James's Todo List </h1>
      </header>
      <Form setInputtext = {setInputtext}/>
      <TodoList />
